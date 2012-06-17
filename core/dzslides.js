f9a90d46a5e04cc8bc142614625ed06037765eb2
Dan Allen <dan.j.allen@gmail.com>
2012-06-17 02:11:50 -0400

    add keybindings space = forward, backspace = back
    - add keybindings for space and backspace
    - optimize conditions
    - rename 'Pop-up' button to 'Launch'
    - add keybinding 'o' to launch pop-up from onstage shell

--- core/dzslides.js
+++ core/dzslides.js
@@ -38,6 +38,11 @@ Dz.setupParams = function() {
 }
 
 Dz.onkeydown = function(aEvent) {
+  // skip keystrokes in a content editable region
+  if (aEvent.target.isContentEditable) {
+    return;
+  }
+
   // Don't intercept keyboard shortcuts
   if (aEvent.altKey
     || aEvent.ctrlKey
@@ -48,34 +53,36 @@ Dz.onkeydown = function(aEvent) {
   if ( aEvent.keyCode == 37 // left arrow
     || aEvent.keyCode == 38 // up arrow
     || aEvent.keyCode == 33 // page up
+    || aEvent.keyCode == 8 // backspace
   ) {
     aEvent.preventDefault();
     this.back();
   }
-  if ( aEvent.keyCode == 39 // right arrow
+  else if ( aEvent.keyCode == 39 // right arrow
     || aEvent.keyCode == 40 // down arrow
     || aEvent.keyCode == 34 // page down
+    || aEvent.keyCode == 32 // space
   ) {
     aEvent.preventDefault();
     this.forward();
   }
-  if (aEvent.keyCode == 35) { // end
+  else if (aEvent.keyCode == 35) { // end
     aEvent.preventDefault();
     this.goEnd();
   }
-  if (aEvent.keyCode == 36) { // home
+  else if (aEvent.keyCode == 36) { // home
     aEvent.preventDefault();
     this.goStart();
   }
-  if (aEvent.keyCode == 32) { // space
+  else if (aEvent.keyCode == 80) { // p
     aEvent.preventDefault();
     this.toggleContent();
   }
-  if (aEvent.keyCode == 70) { // f
+  else if (aEvent.keyCode == 70) { // f
     aEvent.preventDefault();
     this.goFullscreen();
   }
-  if (aEvent.keyCode == 79) { // o
+  else if (aEvent.keyCode == 79) { // o
     aEvent.preventDefault();
     this.toggleView();
   }
